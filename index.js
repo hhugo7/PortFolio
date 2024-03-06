b6f572745c118a5874d865226a9081150e3e69ce
Hugo Hochart <hugo.hochart@ecoles-epsi.net>
2024-03-06 09:09:35 +0100

    ajout du projet

---
+++ index.js
@@ -0,0 +1,8 @@
+const tl = gsap.timeline();
+
+tl.to(".steps", {
+  y: "-950px",
+  duration: 20,
+  repeat: -1,
+  ease: "none"
+});
---
+++ pp2/pp2.js
@@ -0,0 +1,20 @@
+var i = 0;
+var txt = 'Les projets de deuxième année     '; 
+var speed = 150;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("demo").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    } else {
+        // Réinitialiser i pour permettre la répétition de l'animation
+        i = 0;
+        // Effacer le contenu du paragraphe pour la prochaine itération
+        document.getElementById("demo").innerHTML = "";
+        // Appeler à nouveau typeWriter après un délai
+        setTimeout(typeWriter, speed);
+    }
+}
+    
+typeWriter();
